f43a5873b889f473e37be811299b43b81b800c35
Eric King <eric@devadept.com>
2021-03-02 20:24:42 -0700

    Add ReduxDevTools middleware

--- BlazorWithFluxor/Client/Program.cs
+++ BlazorWithFluxor/Client/Program.cs
@@ -14,7 +14,7 @@ namespace BlazorWithFluxor.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddFluxor(o => o.ScanAssemblies(typeof(Program).Assembly));
+            builder.Services.AddFluxor(o => o.ScanAssemblies(typeof(Program).Assembly).UseReduxDevTools());
             await builder.Build().RunAsync();
         }
     }
