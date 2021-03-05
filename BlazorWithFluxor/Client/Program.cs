92f59438c939a3c4676edc4caa4035b1b111b5b7
Eric King <eric@devadept.com>
2021-03-04 20:56:42 -0700

    Use IActionSubscriber to invoke Blazored.Toast when Weather Forecasts are updated.

--- BlazorWithFluxor/Client/Program.cs
+++ BlazorWithFluxor/Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Fluxor;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +16,7 @@ namespace BlazorWithFluxor.Client
             builder.RootComponents.Add<App>("#app");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddFluxor(o => o.ScanAssemblies(typeof(Program).Assembly).UseReduxDevTools());
+            builder.Services.AddBlazoredToast();
             await builder.Build().RunAsync();
         }
     }
